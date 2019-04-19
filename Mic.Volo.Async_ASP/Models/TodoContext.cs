3e650bec136267c3b94025618cc68f537e840929
User <artyom.hovakimyan01@gmail.com>
2019-04-20 01:26:02 +0400

    add framework

---
+++ Mic.Volo.Async_ASP/Models/TodoContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mic.Volo.Async_ASP.Models
+{
+    public class TodoContext:DbContext
+    {
+        public DbSet<ToDo> ToDos { get; set; }
+    }
+}
