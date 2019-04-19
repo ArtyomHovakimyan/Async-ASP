3c542dc698ecd62087645758e07ba1f12d530997
User <artyom.hovakimyan01@gmail.com>
2019-04-20 01:36:20 +0400

    add async method

--- Mic.Volo.Async_ASP/Controllers/HomeController.cs
+++ Mic.Volo.Async_ASP/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Mic.Volo.Async_ASP.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,8 +11,11 @@ namespace Mic.Volo.Async_ASP.Controllers
 {
     public class HomeController : Controller
     {
+        TodoContext db = new TodoContext();
         public ActionResult Index()
         {
+            IEnumerable<ToDo> toDos =  db.ToDos.ToList();
+            ViewBag.Todos = toDos;
             return View();
         }
 
@@ -18,6 +24,12 @@ namespace Mic.Volo.Async_ASP.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+        public async Task<ActionResult> TodoList()
+        {
+            IEnumerable<ToDo> toDos = await db.ToDos.ToListAsync();
+            ViewBag.Todos = toDos;
+            return View("Index");
         }
 
         public ActionResult Contact()
